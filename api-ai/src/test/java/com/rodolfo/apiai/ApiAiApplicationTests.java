cda81abad5474bbfe354ae43d58b8158007dde41
Rodolfo Guerra <rodolfoguerraster@gmail.com>
2025-04-30 22:19:13 -0300

    feat: create project spring-ai with openai

---
+++ api-ai/src/test/java/com/rodolfo/apiai/ApiAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rodolfo.apiai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiAiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
